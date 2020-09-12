d77b07f20a62f172a9d4f3f28afd158c081c93a0
BVPlaten <bernd.von.platen@gmail.com>
2020-09-12 17:22:15 +0200

    first initial commit

---
+++ eventhandling/eventhandling/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace eventhandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
